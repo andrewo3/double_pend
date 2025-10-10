d090d9bbe6512e5689a0fdc4cb08020b187665d4
Andrew Ogundimu <andrew.ogundimu@gmail.com>
2025-10-10 14:56:46 -0400

    velocity scale to stabilize total energy

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import pendulum
 import pygame
 import numpy as np
 import os
+from copy import deepcopy
 
 
 def main():
+    pygame.init()
     WIDTH = 720
     HEIGHT = 1280
     fWIDTH = 1080
@@ -15,8 +17,11 @@ def main():
     cwidth = WIDTH
     cheight = HEIGHT
     fullscreen = False
-    dp = pendulum.DoublePend(200,200,1,1,-0.5,0,0,0)
+    start = pendulum.DoublePend(50,150,1,3,0,np.pi/2,0,0)
+    dp = deepcopy(start)
 
+    sim_acc = 2
+    speed = 1
     clock = pygame.time.Clock()
 
     lines = [(cwidth//2,cheight//2)]
@@ -24,16 +29,18 @@ def main():
     lines.append(lines[-1])
     lines.append((lines[-1][0]+dp.l2*np.sin(dp.a2), lines[-1][1] - dp.l2*np.cos(dp.a2)))
 
+    arial = pygame.font.SysFont('Arial',20)
     while running:
         lines = [(cwidth//2,cheight//2)]
         lines.append((lines[-1][0] + dp.l1*np.sin(dp.a1), lines[-1][1] - dp.l1*np.cos(dp.a1)))
         lines.append(lines[-1])
         lines.append((lines[-1][0]+dp.l2*np.sin(dp.a2), lines[-1][1] - dp.l2*np.cos(dp.a2)))
         pygame.draw.lines(window,(255,255,255),False,lines,5)
+        window.blit(arial.render(f"Total Energy: {dp.E} J",True,(255,255,255)),(0,0))
         pygame.display.update()
         ms = clock.tick(60)
-        for m in range(ms):
-            dp.update(t=0.001)
+        for m in range(sim_acc*speed*ms):
+            dp.update(t=0.001/sim_acc)
         window.fill((0,0,0))
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -47,7 +54,8 @@ def main():
                         os.environ['SDL_VIDEO_WINDOW_POS'] = "0,0"
                     window = pygame.display.set_mode((fWIDTH,fHEIGHT) if fullscreen else (WIDTH, HEIGHT),fullscreen*pygame.NOFRAME)
                     cwidth, cheight = (fWIDTH,fHEIGHT) if fullscreen else (WIDTH, HEIGHT)
-
+                if event.key == pygame.K_r:
+                    dp = deepcopy(start)
 if __name__ == "__main__":
     main()
     quit()
--- pendulum.py
+++ pendulum.py
@@ -12,7 +12,9 @@ class DoublePend():
         self.ad2 = v2
         self.add1 = 0
         self.add2 = 0
-        self.g = 200
+        self.g = 981
+        self.E = None
+        self.startE = None
     def update(self,t = 0.001):
         t1 = -0.5*self.g*np.sin(self.a2)
         t2 = -0.5*self.l1*(self.ad1**2)*np.sin(self.a1-self.a2)
@@ -29,6 +31,26 @@ class DoublePend():
         t3 = 0.5*self.l1*self.add1*np.cos(self.a1-self.a2)
 
         self.add2 = 3/self.l2*(-t1-t2-t3)
+
+        #calc total energy
+        vx1 = -self.l1/2*self.ad1*np.cos(self.a1)
+        vy1 = -self.l1/2*self.ad1*np.sin(self.a1)
+        vx2 = 2*vx1 - self.l2/2*self.ad2*np.cos(self.a2)
+        vy2 = 2*vy1 - self.l2/2 * self.ad2 * np.sin(self.a2)
+        k1 = 1/6*self.m1*(self.l1**2)*(self.ad1**2)
+        k2 = 0.5*self.m2*(vx2**2 + vy2**2) + 1/24*self.m2*(self.l2**2)*(self.ad2**2)
+        u1 = self.m1*self.g*self.l1/2*np.cos(self.a1)
+        u2 = self.m2*self.g*(self.l1*np.cos(self.a1)+0.5*self.l2*np.cos(self.a2))
+        
+        self.E = k1 + k2 + u1 + u2
+        if self.startE is None:
+            self.startE = self.E
+        else:
+            #slightly inaccurate velocity correction to maintain total energy
+            k = np.sqrt((self.startE - u1 - u2)/(k1+k2))
+            self.ad1 *= k
+            self.ad2 *= k
+        
         self.ad1 += self.add1 * t
         self.ad2 += self.add2 * t
         
@@ -36,3 +58,5 @@ class DoublePend():
         self.a2 += self.ad2 * t
         self.a1 %= 2 * np.pi
         self.a2 %= 2 * np.pi
+
+        
